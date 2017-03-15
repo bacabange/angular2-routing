bc3d2b57be2377784164e140e780d8b4787f881d
Stiven Castillo <stivencastillo.90@gmail.com>
2017-03-15 18:09:46 -0500

    Router links

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,33 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  styles: [`
-    
-  `],
-  template: `
-    <header>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">Brand</a>
-          </div>
-
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-  `
+  templateUrl: './app/app.component.html' 
 })
+
 export class AppComponent {
   message = 'This is the sample message.';
 }
